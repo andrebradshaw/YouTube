43e19fb58e0b0b3e3c81c2b4421c307ee887aa22
Andre Bradshaw <bradshaw.andre@gmail.com>
2020-12-22 15:21:12 -0500

    Update commentsMiner.js

--- commentsMiner.js
+++ commentsMiner.js
@@ -8,12 +8,34 @@
                Does not yet pull replies in comments.
       **********************************************************************************************************************************
 */
-async function getCommentsOnVideo(){
+async function getCommentsOnVideo(){    
   var ele = (t) => document.createElement(t);
   var attr = (o, k, v) => o.setAttribute(k, v);
   var reChar = (s) => s.match(/&#.+?;/g) && s.match(/&#.+?;/g).length > 0 ? s.match(/&#.+?;/g).map(el=> [el,String.fromCharCode(/d+/.exec(el)[0])]).map(m=> s = s.replace(new RegExp(m[0], 'i'), m[1])).pop() : s;
   var a = (l, r) => r.forEach(a => attr(l, a[0], a[1]));
- function convert2TsvAndDownload(records, named_file){
+    function dateString(d){
+      var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+      var date = new Date(d);
+      return `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`;
+    }
+    function processTimestampFromXAgo(str){
+        const now = new Date().getTime();
+        var timestamp_;
+        const query = [{x:/year/i,t:3.154e+10},
+        {x:/month/i,t:2.628e+9},
+        {x:/week/i,t:6.048e+8},
+        {x:/day/i,t:8.64e+7},
+        {x:/hour/i,t:3.6e+6},
+        {x:/minute/i,t:60000},
+        {x:/second/i,t:1000},
+        {x:/just.+?now/i,t:1}]
+        for(let i=0; i<query.length; i++){
+            if(query[i].x.test(str)) {
+                return dateString((now - (/\d/.test(str) ? parseInt(str.replace(/\D+/g,'')) * query[i].t : 0)));
+            }
+        }
+    }
+     function convert2TsvAndDownload(records, named_file){
       var fileArray = records;
       var tsvReady = (s) => s ? s.replace(/\t|\u0009|&#9;/g, ' ').replace(/[\r\n]+/g, '↵').replace(/\u2029|\u2028|\x85|\x1e|\x1d|\x1c|\x0c|\x0b/g,'↵').replace(/"/g, "'") : s;
       var unqHsh = (a, o) => a.filter(i => o.hasOwnProperty(i) ? false : (o[i] = true));
@@ -96,7 +118,7 @@ async function getCommentsOnVideo(){
             if(res.status > 899) {return {action: 'stop', status: res.status};}
         } else {return false;}
     }
-    function getYouTubeCommentsThread(){
+    function getYouTubeTokens(){
         const reg = (o, n) => o ? o[n] : '';
         return {
             continuation: encodeURIComponent(reg(/(?<=\{\"continuation\"\:\").+?(?=")/i.exec(document.body.innerHTML),0)),
@@ -108,7 +130,7 @@ async function getCommentsOnVideo(){
         }
     }
     async function getComments(obj){
-        const {continuation,click_tracking,xsrf_token,id_token,page_label,page_cl} = {...getYouTubeCommentsThread(),...obj};
+        const {continuation,click_tracking,xsrf_token,id_token,page_label,page_cl} = {...getYouTubeTokens(),...obj};
         const d = await handleFetch(`https://www.youtube.com/comment_service_ajax?action_get_comments=1&pbj=1&ctoken=${continuation}&continuation=${continuation}&itct=${click_tracking}`, {
       "headers": {
         "accept": "*/*",
@@ -137,35 +159,60 @@ async function getCommentsOnVideo(){
       "mode": "cors",
       "credentials": "include"
     },'json');
+/*
+TODO: need a function to handle time-offset for global users. Need to identify the Google Location Strings, e.g., "America/New York"
+(-(new Date().getTimezoneOffset())).toString()
+*/
         if(d && d.response){
-        return {...parseCommentResponse(d),...{video_title: d.response?.continuationContents?.itemSectionContinuation?.contents[0].commentThreadRenderer?.commentTargetTitle?.simpleText,total_comments:parseInt(d?.response?.continuationContents?.itemSectionContinuation?.header?.commentsHeaderRenderer?.countText?.runs[0].text.replace(/\D+/g,''))}};
+        const reply_context = getCommentRepliesContinuationContext(d);
+        const comments = parseCommentResponse(d);
+        return {...comments,...{reply_context: reply_context, video_title: d.response?.continuationContents?.itemSectionContinuation?.contents[0].commentThreadRenderer?.commentTargetTitle?.simpleText,total_comments:parseInt(d?.response?.continuationContents?.itemSectionContinuation?.header?.commentsHeaderRenderer?.countText?.runs[0].text.replace(/\D+/g,''))}};
         }else{ return false; }
     }
 
+
     function parseCommentResponse(d){
         const tsvReady = (s) => s ? s?.replace(/\t|\u0009|&#9;/g, ' ').replace(/[\r\n]+/g, ' _ ').replace(/\u2029|\u2028|\x85|\x1e|\x1d|\x1c|\x0c|\x0b/g,' _ ').replace(/"/g, "'") : s;
-        const content = d.response.continuationContents.itemSectionContinuation;
+        const content = d.response?.continuationContents?.itemSectionContinuation || d.response?.continuationContents?.commentRepliesContinuation;
         return {
-            comments: content.contents.map(comment=> comment.commentThreadRenderer).map(thread=> {
-                let comment = thread?.comment;
+            comments: content.contents.map(comment=> comment.commentThreadRenderer || comment).map(thread=> {
+                let comment = thread?.comment || thread;
                 let txt = comment?.commentRenderer?.contentText?.runs?.map(text=> text.text);
                 return {
                     video_url: window.location.href,
                     author_name: tsvReady(comment?.commentRenderer?.authorText?.simpleText),
-                    author_channel: comment?.commentRenderer?.authorEndpoint?.browseEndpoint?.canonicalBaseUrl ? 'https://www.youtube.com/'+comment?.commentRenderer?.authorEndpoint?.browseEndpoint?.canonicalBaseUrl : '',
-                    text: txt.length ? tsvReady(txt.reduce((a,b)=> a+' '+b)) : '',
+                    author_channel: comment?.commentRenderer?.authorEndpoint?.browseEndpoint?.canonicalBaseUrl ? 'https://www.youtube.com'+comment?.commentRenderer?.authorEndpoint?.browseEndpoint?.canonicalBaseUrl : '',
+                    text: txt?.length ? tsvReady(txt.reduce((a,b)=> a+' '+b)) : '',
                     num_likes: comment?.commentRenderer?.likeCount ? comment?.commentRenderer?.likeCount : 0,
                     num_replies: comment?.commentRenderer?.replyCount ? comment?.commentRenderer?.replyCount : 0,
                     author_img: comment?.commentRenderer?.authorThumbnail?.thumbnails[comment?.commentRenderer?.authorThumbnail?.thumbnails.length-1].url,
-                    comment_time: comment?.commentRenderer?.publishedTimeText?.runs?.length ? comment?.commentRenderer?.publishedTimeText?.runs?.map(r=> r?.text)[0] : '',
+                    comment_date: comment?.commentRenderer?.publishedTimeText?.runs?.length ? processTimestampFromXAgo(comment?.commentRenderer?.publishedTimeText?.runs?.map(r=> r?.text)[0]) : '',
+                    comment_id: comment?.commentRenderer?.commentId,
                 }
             }),
-            continuation: content.continuations[0].nextContinuationData.continuation,
-            click_tracking: content.continuations[0].nextContinuationData.clickTrackingParams,
+            continuation: content.continuations && content.continuations[0] && content.continuations[0].nextContinuationData ? content.continuations[0].nextContinuationData.continuation : null,
+            click_tracking: content.continuations && content.continuations[0] && content.continuations[0].nextContinuationData ? content.continuations[0].nextContinuationData.clickTrackingParams : null,
         }
+    }
+    function getCommentRepliesContinuationContext(d){
+        const tsvReady = (s) => s ? s?.replace(/\t|\u0009|&#9;/g, ' ').replace(/[\r\n]+/g, ' _ ').replace(/\u2029|\u2028|\x85|\x1e|\x1d|\x1c|\x0c|\x0b/g,' _ ').replace(/"/g, "'") : s;
+        return d?.response?.continuationContents?.itemSectionContinuation?.contents?.map(content=> {
+            let render = content?.commentThreadRenderer;
+            let replies = render?.replies?.commentRepliesRenderer;
+            let continuations = replies?.continuations && replies?.continuations[0] && replies?.continuations[0].nextContinuationData;
+            let txt = render?.comment?.commentRenderer?.contentText?.runs?.map(text=> text.text);
+            return {
+                num_replies: render?.comment?.commentRenderer?.replyCount ? render?.comment?.commentRenderer?.replyCount : 0,
+                comment_replied_to_text: txt?.length ? tsvReady(txt.reduce((a,b)=> a+' '+b)) : '',
+                comment_replied_to_id: render?.comment?.commentRenderer?.commentId,
+                click_tracking: continuations?.clickTrackingParams ? encodeURIComponent(continuations?.clickTrackingParams) : '',
+                continuation: continuations?.continuation ? encodeURIComponent(continuations?.continuation) : '',
+            };
+        })
     }
 
     async function loopThroughComments(){
+        function unqKey(array,key){  var q = [];  var map = new Map();  for (const item of array) {    if(!map.has(item[key])){        map.set(item[key], true);        q.push(item);    }  }  return q;}
         createDownloadHTML();
         const everyNth = (i,n) => /\./.test((i/n).toString()) === false && i != 0;
         const reg = (o, n) => o ? o[n] : '';
@@ -176,6 +223,7 @@ async function getCommentsOnVideo(){
         var continuation = first.continuation;
         var click_tracking = first.click_tracking;
         const contain_arr = [first.comments];
+        const replies_container = [first.reply_context];
         for(let i=0; i<total_comments; i=i+20){
             let comments = await getComments({click_tracking:click_tracking,continuation:continuation});
             if(comments){
@@ -183,15 +231,52 @@ async function getCommentsOnVideo(){
                 click_tracking = comments.click_tracking;
                 await delay(rando(333)+333);
                 contain_arr.push(comments.comments);
+                replies_container.push(comments.reply_context);
                 updateDownloadBar({text:' seconds remaining.',seconds_offset:1.91,iteration:i,total_results:total_comments,status:true});
-                if(everyNth(i,200)) await delay(rando(1333)+1333);
+                if(everyNth(i,200)) {await delay(rando(1333)+1333); console.log(i)}
             }else{
                 break;
             }
         }
+        const replies = await loopThroughReplies(replies_container.flat().filter(r=> r.num_replies > 0),total_comments,contain_arr.flat().length);
+        const output = [...contain_arr.flat(),...replies];
         updateDownloadBar({text:'',seconds_offset:0.5,iteration:100,total_results:100,status:false});
-        return {file: contain_arr.flat(),filename:first.video_title.replace(/\./g,' ').trim()};
+        return {file: output,filename:first.video_title.replace(/\./g,' ').trim()};
     }
+    async function loopThroughReplies(arr,total_comments,progress){
+        const everyNth = (i,n) => /\./.test((i/n).toString()) === false && i != 0;
+        const contain_arr = [];
+        var continuation;
+        var click_tracking;
+        for(let i=0; i<arr.length; i++){
+            let reply_context = {comment_replied_to_id: arr[i].comment_replied_to_id,comment_replied_to_text:arr[i].comment_replied_to_text};
+            let comments = await getComments({click_tracking:arr[i].click_tracking,continuation:arr[i].continuation});
+            if(comments){
+                comments.comments?.forEach(comment=> contain_arr.push({...comment,...reply_context}));
+                if(comments.num_replies > 20){
+                    console.log(['comments.num_replies',comments.num_replies]);
+                    continuation = comments.continuation;
+                    click_tracking = comments.click_tracking;
+                    for(let r=0; r<comments.num_replies; r=r+20){
+                        let replies = await getComments({click_tracking:click_tracking,continuation:continuation});
+                        if(replies){
+                            replies.comments?.forEach(comment=> contain_arr.push({...comment,...reply_context}));
+                            continuation = replies.continuation;
+                            click_tracking = replies.click_tracking;
+                            await delay(rando(333)+333);
+                        }else{ break; } 
+                    }
+                }
+                if(everyNth(i,200)) await delay(rando(2333)+1333);
+            }else{
+                break;
+            }
+            updateDownloadBar({text:' seconds remaining.',seconds_offset:1.91,iteration:(progress+i),total_results:total_comments,status:true});
+
+        }
+        return contain_arr;
+    }
+
 
     function createDownloadHTML() {
         const gi = (o, s) => o ? o.getElementById(s) : null;
@@ -224,5 +309,6 @@ async function getCommentsOnVideo(){
     }
     const all_comments = await loopThroughComments();
     convert2TsvAndDownload(all_comments.file,`${all_comments.filename}.tsv`);
+
 }
 getCommentsOnVideo()
